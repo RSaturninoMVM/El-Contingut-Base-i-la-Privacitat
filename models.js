7d909b10eba728587b5970d2c01ff49edc350caf
«Ruben» <rsaturnino@institutmvm.cat>
2026-02-03 12:26:09 +0100

    first commit

---
+++ models.js
@@ -0,0 +1,18 @@
+class Multimedia{
+    Constructor(titol, durada, views) {
+        this.titol = titol;
+        this.durada = 60;
+        this.views = 0;
+    }
+    play() {
+        console.log("Reproduint ...");
+        this.views + 1;
+    }
+    getViews() {
+        return this.views;
+    }
+    calcularCostServer() {
+        this.durada * 0.01;
+    }
+}
+    
