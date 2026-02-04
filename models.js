1929882b7bbdbe24c2b5aaa08451358b4f594124
Zara Nasir <zaranasir392@gmail.com>
2026-02-04 11:38:40 +0100

    first commit

--- models.js
+++ models.js
@@ -1,17 +1,17 @@
 class Multimedia{
-    Constructor(titol, durada, views) {
+    Constructor( titol, durada, #views) {
         this.titol = titol;
         this.durada = 60;
-        this.views = 0;
+        this.#views = 0;
     }
     play() {
         console.log("Reproduint ...");
-        return (this.views + 1);
+        return (this.#views + 1);
     }
     getViews() {
-        return this.views;
+        return this.#views;
     }
-    calcularCostServer() {
+    #calcularCostServer() {
         return (this.durada * 0.01);
     }
 }
